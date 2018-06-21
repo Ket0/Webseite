d7d1827536218b5bc8e1fca73f656f8ae1ac0555
Ket0 <36997534+Ket0@users.noreply.github.com>
2018-06-21 17:04:52 +0200

    Create php_information.php

---
+++ php_information.php
@@ -0,0 +1,15 @@
+# PHP Info vom Server
+
+<!DOCTYPE html>
+<html>
+	<head lang="de">
+		<meta charset="UTF-8">
+		<title>Testseite für PHP</title>
+	</head>
+	<body>
+		<?php
+			echo "hello from php";
+			phpinfo();
+		?>
+</body>
+</html>	
